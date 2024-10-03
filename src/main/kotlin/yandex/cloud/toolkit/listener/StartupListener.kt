8bd787315b44bc36b48c41e4f7ae0ac33c391883
Roman Kasatov <romankasatov@yandex-team.ru>
2024-10-03 14:27:54 +0300

    remove redundant lines

--- src/main/kotlin/yandex/cloud/toolkit/listener/StartupListener.kt
+++ src/main/kotlin/yandex/cloud/toolkit/listener/StartupListener.kt
@@ -1,11 +1,5 @@
 package yandex.cloud.toolkit.listener
 
 import com.intellij.ide.AppLifecycleListener
-import io.grpc.LoadBalancerRegistry
-import io.grpc.util.OutlierDetectionLoadBalancerProvider
 
-class StartupListener : AppLifecycleListener {
-    override fun appStarted() {
-        LoadBalancerRegistry.getDefaultRegistry().register(OutlierDetectionLoadBalancerProvider())
-    }
-}
+class StartupListener : AppLifecycleListener
